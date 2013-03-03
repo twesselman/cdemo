2f27c5831567ecdf4daab733320389ea2ed913f3
Tom Wesselman <twesselman@gmail.com>
2013-03-03 15:21:41 -0500

    j

--- server.js
+++ server.js
@@ -4,15 +4,12 @@ var http = require ('http');
 
 app.use(express.cookieParser());
 app.use(express.cookieSession({ secret: 'Calder Demo Secret', cookie: { maxAge: 60 * 60 * 1000 }}));
- 
-var ericomurl='http://10.81.108.10:8080/AccessNow/start.html';
 
 app.configure(function() {
     app.use(express.bodyParser());
     app.use(express.cookieParser());
     app.use(express.session({secret: 'my super secret'}));
     app.use(express.static(__dirname + '/website'));    
-    app.use(express.static(__dirname + '/kendo'));
     app.use(express.static(__dirname + '/website/script'));
     app.use(express.static(__dirname + '/website/content'));
     });
@@ -43,47 +40,27 @@ app.get('/workspace', function(req, res) {
     console.log('/workspace')
     console.log('id: '+req.query.csuserid);
     console.log('manager:'+req.query.csmanager);
-    req.session.csuserid = req.query.csuserid;
-    req.session.csmanager = req.query.csmanager;
+    if (req.query.csmanager)
+    {
+        req.session.csuserid = req.query.csuserid;
+        req.session.csmanager = req.query.csmanager;
+    }
     
-    res.redirect('/calder.html')
+    res.redirect('/calder.html');
 });
 
-var myxxxxxApps = {"apps": [
-        {"name": "Outlook", "imagefile": "outlook.png", "exe": "/launch/outlook"},        
-        {"name": "Word", "imagefile": "word.png", "exe": "/launch/word"},        
-        {"name": "Excel", "imagefile": "excel.png", "exe": "/launch/excel"},        
-        {"name": "Powerpoint", "imagefile": "powerpoint.png", "exe": "/launch/msppt"},        
-        {"name": "Paint", "imagefile": "mspaint.png", "exe": "/launch/mspaint"},        
-        {"name": "Aptana", "imagefile": "aptana.png", "exe": "/launch/aptana"},
-        {"name": "Autocad", "imagefile": "autocad.png", "exe": "#"},
-        {"name": "SAP ERP", "imagefile": "erp.png", "exe": "#"}
+var myAppsBackup = {"apps": [
+        {"name": "Paint (local)", "imagefile": "mspaint.png", "exe": "/launch/mspaint"}    
     ]
 };
-
-var myApps = {"apps": [
-        {"name": "Paint", "imagefile": "mspaint.png", "exe": "/launch/mspaint"}    
-    ]
-};
-
-app.get('/myapps2', function(req, res) {
-    console.log('/myapps2 - local');
-    res.send(JSON.stringify(myApps));
-    });
-
     
 app.get('/myapps', function(req, res) {
     console.log('/myapps');
 
-    
-// *********************change to use real host - req.session.csmanager,
-//path: '/myapps?id='+req.session.csuserid,
-//          host: 'http://10.81.108.14',
-
     if (req.session.csmanager) {
         var options = {
-          host: '10.81.108.14',
-          path: '/myapps',
+          host: req.session.csmanager,
+          path: '/myapps?id='+req.session.csuserid,
           port: 3000,
           method: 'GET'
         };
@@ -110,50 +87,57 @@ app.get('/myapps', function(req, res) {
     }
     else
     {
-        res.send(JSON.stringify(myApps));
+        res.send(JSON.stringify(myAppsBackup));
     }
 });
 
 app.get('/launch/:app', function(req, res) {
-    console.log('launch called: App: '+req.params.app);
-    
-    // set cookies (works only if Ericom is on the same server
-
-    res.cookie('EAN_username', 'Tom');
-    res.cookie('EAN_password', 'Cisco123!');
-    res.cookie('EAN_autostart', 'true');
-    res.cookie('EAN_remoteapplicationmode', 'true');
-
-    var appPath;
-    switch (req.params.app) {
-    case 'aptana': 
-        appPath = '"C:\\Users\\Administrator\\AppData\\Local\\Aptana Studio 3\\AptanaStudio3.exe"';
-        break;
-    case 'desktop': 
-        res.cookie('EAN_remoteapplicationmode', 'false');
-        console.log('Launching: desktop');
-        break;
-    default: 
-        appPath = req.params.app;
-    
+    if (req.session.csmanager)
+    {
+        console.log('oops');
     }
-
-    if (appPath) {
-        res.cookie('EAN_alternate_shell', appPath);
-        console.log('Launching: ' + appPath);
-    } 
+    else
+    {
+        console.log('launch called: App: '+req.params.app);
+        
+        // set cookies (works only if Ericom is on the same server
     
-    var url = ericomurl;
-    if (appPath) {
-        url += '?autostart=true&remoteapplicationmode=true&username=Tom&password=Cisco123!&alternate_shell=';
-        url += appPath;        
-    }    
-    else {
-        url += '?autostart=true&username=Tom&password=Cisco123!';
+        res.cookie('EAN_username', 'Tom');
+        res.cookie('EAN_password', 'Cisco123!');
+        res.cookie('EAN_autostart', 'true');
+        res.cookie('EAN_remoteapplicationmode', 'true');
+    
+        var appPath;
+        switch (req.params.app) {
+        case 'aptana': 
+            appPath = '"C:\\Users\\Administrator\\AppData\\Local\\Aptana Studio 3\\AptanaStudio3.exe"';
+            break;
+        case 'desktop': 
+            res.cookie('EAN_remoteapplicationmode', 'false');
+            console.log('Launching: desktop');
+            break;
+        default: 
+            appPath = req.params.app;
+        
+        }
+    
+        if (appPath) {
+            res.cookie('EAN_alternate_shell', appPath);
+            console.log('Launching: ' + appPath);
+        } 
+        
+        var url = ericomurl;
+        if (appPath) {
+            url += '?autostart=true&remoteapplicationmode=true&username=Tom&password=Cisco123!&alternate_shell=';
+            url += appPath;        
+        }    
+        else {
+            url += '?autostart=true&username=Tom&password=Cisco123!';
+        }
+        
+        console.log(url);
+        res.redirect(url);
     }
-    
-    console.log(url);
-    res.redirect(url);
 
 //    res.redirect(ericomurl);
 });
